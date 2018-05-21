08b0963b22775dd71e722088f6f3f61ea56d23f9
Adrian <admd911@hotmail.com>
2018-05-21 17:36:23 +0100

    Stats function updated to include more statistics about texts.
    Signed-off-by: Adrian <admd911@hotmail.com>

--- Get_Stats.py
+++ Get_Stats.py
@@ -1,11 +1,13 @@
-"""Counts (Irish) texts on main page of CELT, translations, Categories, Subcategories.
-   Add: subcategory per category and text per subcategory.
+"""Counts (Irish) texts on main page of CELT, translations, Categories, Subcategories, subcategories per category
+   and lists them all..
+   Add: text per subcategory.
    Old one could get authorship known and unknown count (poorly), this can be done with a secondary stat function
    to count output from Celt_Crawl function.
    TO DO: Find out if broken links are included in 'Number of texts' by counting links in category/subcategory"""
 
 
 from Site_Open import siteopen
+from bs4 import BeautifulSoup
 
 
 def getstats(url):
@@ -68,17 +70,22 @@ def getstats(url):
             # print(frtextinfo)
     """Finds and counts categories"""
     categories = []
+    catnames = []
     for category in html.find_all('h4'):
         catcount += 1
         catname = category.text
+        catname = (str(catname))
+        catnames.append(catname)
         catinfo = ("   " + str(catcount) + ". " + catname)
         categories.append(catinfo)
     """Finds and counts subcategories"""
     subcategories = []
+    subcatnames = []
     for subcat in html.find_all('p'):
         subcatname = subcat.text
         subcatname = (str(subcatname))
         if "Corpus of Electronic Texts" not in subcatname:
+            subcatnames.append(subcatname)
             subcatcount += 1
             replaces = {"\r": "", "\n": " "}
             for item in replaces:
@@ -87,19 +94,71 @@ def getstats(url):
                     for i in range(0, itemcount):
                         itemplace = subcatname.find(item)
                         subcatname = subcatname[:itemplace] + replaces.get(item) + subcatname[itemplace + len(item):]
-            subcatinfo = ("   " + str(subcatcount) + ". " + subcatname)
+            subcatinfo = ("      " + str(subcatcount) + ". " + subcatname)
             subcategories.append(subcatinfo)
+    """Finds and counts subcategories per category"""
+    catdict = {}
+    for cname in catnames:
+        subcatecount = 0
+        searchtext = (str(html))
+        replacables = {"&": "&amp;"}
+        origname = cname
+        for item in replacables:
+            if item in cname:
+                probpos = cname.find(item)
+                cname = cname[:probpos] + replacables.get(item) + cname[probpos + len(item):]
+        if cname in searchtext:
+            cnamepos = searchtext.find(cname)
+            if origname == catnames[-1]:
+                endpos = None
+            elif origname != catnames[-1]:
+                nextcat = (catnames.index(origname) + 1)
+                nextcatname = catnames[nextcat]
+                for replacable in replacables:
+                    if replacable in nextcatname:
+                        probpos = nextcatname.find(replacable)
+                        nextcatname = nextcatname[:probpos] + \
+                            replacables.get(replacable) + nextcatname[probpos + len(replacable):]
+                endpos = searchtext.find(nextcatname)
+        searchtext = searchtext[cnamepos:endpos]
+        identifier = "<p>"
+        closer = "</p>"
+        pcount = searchtext.count(identifier)
+        subcatchecks = []
+        for i in range(0, pcount):
+            beginpos = searchtext.find(identifier) + 3
+            closepos = searchtext.find(closer)
+            subcattext = searchtext[beginpos:closepos]
+            subcatchecks.append(subcattext)
+            searchtext = searchtext[:beginpos - 3] + searchtext[closepos + 4:]
+        for subcatcheck in subcatchecks:
+            if subcatcheck in subcatnames:
+                subcatecount += 1
+        catdict.update({cname: subcatecount})
     pagestats.append("Number of Texts: " + str(textcount))
     pagestats.append("Number of Translations: " + str(transcount))
     pagestats.append("   English Translations: " + str(engcount))
     pagestats.append("   French Translations: " + str(frcount))
     pagestats.append("Number of Broken Links: " + str(breakcount))
     pagestats.append("Number of Categories: " + str(catcount))
+    pagestats.append("Number of Subcategories: " + str(subcatcount))
+    """Appends Categories and, where applicable, their subcategories to the output list, pagestats, in order"""
     for cat in categories:
         pagestats.append(cat)
-    pagestats.append("Number of Subcategories: " + str(subcatcount))
-    for subcate in subcategories:
-        pagestats.append(subcate)
+        cattitle = (str(cat[6:]))
+        if cattitle in catdict:
+            removables = {"&amp;": "&"}
+            for removable in removables:
+                if removable in cattitle:
+                    remopos = cattitle.find(removable)
+                    cattitle = cattitle[:remopos] + removables.get(removable) + cattitle[remopos + len(removable):]
+            catsubcats = catdict.get(cattitle)
+        if catsubcats > 0:
+            pagestats.append("      Subcategories: " + str(catsubcats))
+            for i in range(0, catsubcats):
+                pagestats.append(subcategories[i])
+            for i in range(0, catsubcats):
+                subcategories.remove(subcategories[0])
     return pagestats
 
 
